6953b2c3b45f369b74764ccc967c4a465e2ae991
shabrat2015 <shabrat2015@gmail.com>
2020-12-25 00:14:57 +0200

    Исправил

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let page = 0;
+let page = -1;
 let pages = document.getElementsByClassName("step-box");
+next_page();
 
 function next_page(){
+	if(page == pages.length-1){
+		alert("Thank you for yoir choise!");
+		return;
+	}
 	page = (page + 1) % pages.length;
-	pages.forEach(function(item, i, arr){
+	for(let item of pages){
 		item.style.display = "none";
-	});
+	}
 	pages[page].style.display = "";
+}
+
+function prev_page(){
+	if(page != 0){
+		page--;
+		for(let item of pages){
+			item.style.display = "none";
+		}
+		pages[page].style.display = "";
+	}
 }
